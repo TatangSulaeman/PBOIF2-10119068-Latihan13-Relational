dca05bbc83a4f1343bf195066be8c8392229b5f6
amtat <amtat@DESKTOP-3DL3458>
2020-10-17 17:48:50 +0700

    PBOIF2-10119068-Latihan13-Relational

---
+++ src/pboif2/pkg10119068/latihan13/relational/PBOIF210119068Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119068.latihan13.relational;
+
+/**
+ *
+ * @author amtat
+ * NAMA     : TATANG SULAEMAN
+ * KELAS    : PBOIF2
+ * NIM      : 10119068
+ * Deskripsi program    : Relational
+ */
+public class PBOIF210119068Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b > a = " + (bil2 >= bil1));
+        System.out.println("b < a = " + (bil2 <= bil1));
+        
+    }
+    
+}
